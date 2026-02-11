7e5cde3d8fee8c8ef47aedeb817515c1b0ad613c
hnnayy <mutiahanin2017@gmail.com>
2026-02-11 20:28:50 +0700

    add

---
+++ Maven/demo/src/main/java/car.example.setter.injection/App.java
@@ -0,0 +1,11 @@
+package car.example.construction.injection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Applicationsetter.xml");
+        Car myCar = (Car) context.getBean("myCar");
+        myCar.displayDetails();
+    }
+}
---
+++ Maven/demo/src/main/java/car.example.setter.injection/Specification.java
@@ -0,0 +1,30 @@
+package car.example.construction.injection;
+
+public class Specification {
+    private String make;
+    private String model;
+
+    public void setMake(String make) {
+        this.make = make;   
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
